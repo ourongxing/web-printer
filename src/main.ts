7a20663b66ec0f123e33c0116eb520e129059e66
ourongxing <orongxing@gmail.com>
2022-12-06 21:32:17 +0800

    chore: minor change

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,31 @@
 import { chromium } from "playwright"
 import { projectRoot } from "~/utils"
+import javascriptInfo from "./javascriptInfo"
 import mdbook from "./mdbook"
 import ruanyfWeekly from "./ruanyfWeekly"
+import { ContextOption } from "./types"
 import vitepress from "./vitepress"
 import xiaobot from "./xiaobot"
 import zhubai from "./zhubai"
 
-async function launchPersistentContext(headless = true) {
+async function launchPersistentContext(options: ContextOption) {
   return await chromium.launchPersistentContext(await projectRoot("userData"), {
-    headless
+    ...options
   })
 }
 
-async function login() {
-  const context = await launchPersistentContext(false)
-  const page = await context.newPage()
-  // await page.goto("https://course.rs/community.html")
+async function test() {
+  const context = await launchPersistentContext({
+    headless: false,
+    userAgent:
+      "Mozilla/5.0 (Linux; Android 13; Pixel 4 XL Build/TPB3.220610.004; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3235 MMWEBSDK/20220402 Mobile Safari/537.36 MMWEBID/4289 MicroMessenger/8.0.22.2140(0x280016F8) WeChat/arm64 Weixin NetType/WIFI Language/en ABI/arm64"
+  })
 }
 
 async function main() {
-  const context = await launchPersistentContext(false)
+  const context = await launchPersistentContext({
+    headless: true
+  })
   // await zhubai("海上星光1", "https://hsxg.zhubai.love", () => true, context, {
   //   margin: { top: 0, left: 0, right: 0, bottom: 0 },
   //   thread: 3
@@ -94,7 +100,13 @@ async function main() {
   //     printBackground: true
   //   }
   // )
+  // await javascriptInfo("现代 JavaScript 教程", () => true, context, {
+  //   margin: { top: 0, left: 30, right: 30, bottom: 0 },
+  //   quality: 144,
+  //   thread: 10,
+  //   printBackground: true
+  // })
 }
 
-main()
-// login()
+// main()
+test()
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,4 @@
-export * from "./print"
+export * from "./playwright"
 type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
 type XOR<T, U> = T | U extends object
   ? (Without<T, U> & U) | (Without<U, T> & T)
