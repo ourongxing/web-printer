53d2a03e0ff241b628ea0619aacccd605f96fb96
Ou <orongxing@gmail.com>
2023-08-10 11:13:51 +0800

    fix: support latest vitepress

--- packages/vitepress/src/index.ts
+++ packages/vitepress/src/index.ts
@@ -23,36 +23,26 @@ export default function (options: {
         const data = JSON.parse(
           await page.evaluate(`
 (() => {
-  function genGroups(nodes, groups) {
-    return nodes.reduce((acc, node, i) => {
-      const paddingLeft = Number(node.style.paddingLeft.replace("px", ""))
-      let selfGroup = false
-      if(i!==0) {
-        const interval = paddingLeft - acc.paddingLeft
-        if(interval > 0) {
-          if(acc.interval === 0) acc.interval = interval
-          acc.groups.push(acc.title)
-          selfGroup = true
-        } else if(interval < 0) {
-          for(let i=0; i<Math.abs(interval/acc.interval); i++) acc.groups.pop()
-        }
-      }
+  const ret = []
+  function d(k, groups) {
+    k.forEach((level0) => {
+      const title = level0.querySelector(":scope > .item .text")?.innerText
+      const url = level0.querySelector(":scope > .item .link")?.href
+      const children = level0.querySelectorAll(":scope > .items > .VPSidebarItem")
+      if (url)
+        ret.push({
+          title,
+          url,
+          collapsed: false,
+          selfGroup: Boolean(title && url && children.length),
+          groups
+        })
+      if (children.length) d(children, [...groups, title].filter(Boolean))
+    })
+  }
+  d(document.querySelectorAll(".level-0"), [])
 
-      acc.title = node.innerText
-      acc.paddingLeft = paddingLeft
-      acc.items.push({title: node.innerText, url: node.href, selfGroup, groups: [...acc.groups]})
-      return acc
-    }, {items:[], paddingLeft:0, title: "", groups, interval: 0}).items.filter(k => k.url)
-  };
-  const ret = [...document.querySelectorAll("#VPSidebarNav .group")].map((k,i)=> {
-      const title = k.querySelector(".title")?.innerText
-      if(i && !title) return []
-      return genGroups([...k.querySelectorAll(".VPSidebarGroup .link")], title ? [{
-        name: title,
-        collapsed: !!k.querySelector(".collapsed")
-      }]:[])
-  }).flat()
-  return JSON.stringify(ret)
+  return JSON.stringify(ret.filter(k => k.title))
 })()
   `)
         ) as PageInfo[]
